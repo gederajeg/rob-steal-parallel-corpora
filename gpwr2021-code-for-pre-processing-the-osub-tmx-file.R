a7da80b4183adbff6e7c486f6fb7386ffc6ada4f
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2021-11-03 21:15:24 +0800

    Add the equality operator to check that the English and Indonesian texts have the same length or the same number of lines or sentences

--- gpwr2021-code-for-pre-processing-the-osub-tmx-file.R
+++ gpwr2021-code-for-pre-processing-the-osub-tmx-file.R
@@ -6,11 +6,12 @@ library(tidyverse)
 
 # check the current directory/folder that we are working with
 getwd()
-# [1] "/Users/Primahadi/Documents/research/2020-11-03-PARALLEL-CORPUS-PUPS" <- my working directory/folder. The output files from this process will be saved in this current directory/folder in our computer (the directory will be different in each computer for each user of this script)
+# [1] "/Users/Primahadi/Documents/research/2020-11-03-PARALLEL-CORPUS-PUPS" <- my working directory/folder. 
+# The output files from this process will be saved in this current directory/folder in our computer (the directory will be different in each computer for each user of this script)
 
 # load into R the Eng-Idn OpenSubtitles corpus in .tmx format
 tmx <- readr::read_lines(file = "~/Downloads/en-id-OpenSubtitles.tmx")
-# The code in line 12 above shows that I put the corpus file in my "Downloads" directory/folder.
+# The code in line 13 above shows that I put the corpus file in my "Downloads" directory/folder.
 # Make sure you know where is the folder path/directory of this .tmx corpus in your computer.
 # Also note that the .tmx corpus of the OpenSubtitles is a large file (1-GB file).
 
@@ -31,10 +32,7 @@ regex_to_extract_en_text <- '(?<=["]en["]\\>\\<seg\\>)[^<]+(?=\\<\\/seg\\>)'
 
 eng <- stringr::str_extract(string = eng, pattern = regex_to_extract_en_text)
 eng <- stringr::str_replace_all(string = eng, pattern = "(^\\-\\=|\\=\\-$)", "")
-head(eng)
-
-# Save the English source language to the current working directory 
-readr::write_lines(x = eng, file = "eng_OpenSub.txt")
+head(eng) # peek into the first six lines of the output
 
 # Extract only the text by removing the tagging (Indonesian section)
 
@@ -43,7 +41,13 @@ regex_to_extract_id_text <- '(?<=["]id["]\\>\\<seg\\>)[^<]+(?=\\<\\/seg\\>)'
 
 idn <- stringr::str_extract(string = idn, pattern = regex_to_extract_id_text)
 idn <- stringr::str_replace_all(string = idn, pattern = "(^\\-\\=|\\=\\-$)", "")
-head(idn)
+head(idn) # peek into the first six lines of the output
+
+# check that the number of lines/sentences should be the same between the English and Indonesian texts
+length(eng) == length(idn) # this equality logical operation should produce TRUE
+
+# Save the English source language to the current working directory 
+readr::write_lines(x = eng, file = "eng_OpenSub.txt")
 
 # Save the Indonesian target language to the current working directory
 readr::write_lines(x = idn, file = "id_OpenSub.txt")
